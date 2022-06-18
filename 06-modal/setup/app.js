49192e967ddcd1e304cbef8e8fae0956ae9c90f4
Orangel <orangabriel18@gmail.com>
2022-06-18 14:15:39 -0400

    Finished "Modal" project

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const openmodal = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const close = document.querySelector('.close-btn');
+
+openmodal.addEventListener('click', function() {
+    modal.classList.add('open-modal');
+});
+
+close.addEventListener('click', function() {
+    modal.classList.remove('open-modal');
+});
