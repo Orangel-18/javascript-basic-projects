78dbc5d5198e440478f0a15889aab7c4ff374d1b
Orangel <orangabriel18@gmail.com>
2022-06-18 13:01:24 -0400

    Finished "Sidebar" project

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector('.sidebar-toggle');
+
+const sidebar = document.querySelector('.sidebar');
+
+const close = document.querySelector('.close-btn');
+
+toggle.addEventListener('click', function() {
+    sidebar.classList.toggle('show-sidebar');
+});
+
+close.addEventListener('click', function() {
+    sidebar.classList.remove('show-sidebar');
+});
